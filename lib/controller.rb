395394c339f2ad5b545ac43b2fd141bcce22181f
ThoMich22 <thomas_8_69@hotmail.com>
2021-05-03 23:05:22 +0200

    third commit

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,10 @@
+
 require 'gossip'
 
 class ApplicationController < Sinatra::Base
   
   get '/' do
-    erb :index, locals: {gossip: Gossip.all}
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get '/gossips/new/' do
@@ -17,18 +18,19 @@ class ApplicationController < Sinatra::Base
 
   get '/gossips/:id/' do
     puts "Voici le numéro du potin choisi: #{params['id']} !"
+    #Gossip.find(params['id'].to_i)
     erb :show, locals: {id: params['id'], gossip: Gossip.find(params['id'].to_i)}
+    #erb :show, locals: {gossip: Gossip.find(params['id'].to_i)
   end
-  
+
   get '/gossips/:id/edit/' do
     puts "Voici le numéro du potin à modifier: #{params['id']} !"
     erb :edit, locals: {id: params['id']}
   end
 
   post '/gossips/:id/edit/' do
-    
+    puts "Voici le numéro du potin à modifier: #{params['id']} !"
     Gossip.update(params['id'], params["gossip_author"], params["gossip_content"])
     redirect '/'
   end
-
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,51 +1,47 @@
-require 'pry'
 require 'csv'
 
-class Gossip 
-    attr_reader :author, :content
+class Gossip
+  attr_accessor :content, :author
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+  
+    CSV.read("./db/gossip.csv").each do |ligne| 
+      all_gossips << Gossip.new(ligne[0], ligne[1])
+    end 
+  
+    return all_gossips
+  end
+
+  def self.find(id)
+    all_gossips = [] #sur le même principe que self.all, on créé un array avec tous les gossips, mais cette fois ci, on ne récupèrera que celui qui nous intéresse
+    CSV.read("./db/gossip.csv").each do |ligne| 
+      all_gossips << Gossip.new(ligne[0], ligne[1])
+    end 
+    return all_gossips[id-1] #le -1 permet de compenser le décalage créé par les headers
+  end
+
+  def self.update(id, gossip_author, gossip_content)
+    all_gossips = self.all
+
+
+    all_gossips[id.to_i-1].author = gossip_author #le -1 permet de compenser le décalage créé par les headers
+    all_gossips[id.to_i-1].content = gossip_content
     
-    def initialize(author, content)
-         @author = author
-         @content = content
-     end
+    CSV.open("./db/gossip.csv", "w") {|csv| all_gossips.each{|gossip| gossip.save}} #on itère sur chaque élément du fichier et CSV, 
+    #et sur chacun de ces éléments on fait une nouvelle sauvegarde de chaque élément : mais le seul qui change est celui modifié ligne 39 et 40
 
-    def save
-        CSV.open("/home/thomasmichonneau/Bureau/THP/THPJ18/db/gossip.csv", "ab") do |csv|
-            csv << [@author, @content]
-        end    
-    end
+  end
 
-    def self.all
-        all_gossips = []
-
-        CSV.read("/home/thomasmichonneau/Bureau/THP/THPJ19/the_gossip_project_sinatra/db/gossip.csv").each do |csv_line|
-          all_gossips << Gossip.new(csv_line[0], csv_line[1])
-        end
-      
-        return all_gossips
-    end
-
-    def self.find(id)
-        all_gossips = []
-        
-        CSV.read("/home/thomasmichonneau/Bureau/THP/THPJ19/the_gossip_project_sinatra/db/gossip.csv").each do |csv_line|
-            all_gossips << Gossip.new(csv_line[0], csv_line[1])
-        end
-
-        return all_gossips[id - 1]
-       
-        
-    end
-
-    def self.update(id, gossip_author, gossip_content)
-        all_gossips = self.all
-
-
-        all_gossips[id.to_i - 1].author = gossip_author #le -1 permet de compenser le décalage créé par les headers
-        all_gossips[id.to_i - 1].content = gossip_content
-    
-        CSV.open("/home/thomasmichonneau/Bureau/THP/THPJ19/the_gossip_project_sinatra/db/gossip.csv", "w") {|csv| all_gossips.each{|gossip| gossip.save}} #on itère sur chaque élément du fichier et CSV, 
-        #et sur chacun de ces éléments on fait une nouvelle sauvegarde de chaque élément : mais le seul qui change est celui modifié ligne 39 et 40
-
-    end
 end
